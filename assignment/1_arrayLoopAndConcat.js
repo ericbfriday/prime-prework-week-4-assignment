708f1271f6988c1e921e9235dbed8cc63c368438
Eric Friday <ericfriday@gmail.com>
2017-08-10 10:49:56 -0500

    Working on assignment 2 - pushing for work on other machine

--- assignment/1_arrayLoopAndConcat.js
+++ assignment/1_arrayLoopAndConcat.js
@@ -1,11 +1,14 @@
 function concatArrayToString(array) {
   var conString = '';
+    for (i = 0; i < (array.length); i ++) {
+      conString += array[i];
+    }
   // Write code here: Concatentate all of the values in array together using a loop
   // For example: if the array given is [1, 2, 3, 4] the conString should be '1234'
-
 
   // return concatentated values
   return conString;
 }
 
 // call concatArrayToString and console log the return
+console.log(concatArrayToString([1,2,3,4]));
--- assignment/2_arrayReverser.js
+++ assignment/2_arrayReverser.js
@@ -1,13 +1,17 @@
+
+// Copy items from the array called arr to a new array called reversedArray, reversing the order of elements
+// The last element on arr should become the first in reversedArray
+// Do not use Array.reverse()!
+
+// Write the code here
+var reversedArray = [];
 function arrayReverser(arr) {
-  // Copy items from the array called arr to a new array called reversedArray, reversing the order of elements
-  // The last element on arr should become the first in reversedArray
-  // Do not use Array.reverse()!
-
-  // Write the code here
-
-
+  for (i = 0; i < arr.length; i++) {
+    reversedArray.unshift(arr[i]);
+  }
   // return our reversed array
   return reversedArray;
-}
+  }
 
 // call arrayReverser and console log the return
+console.log(arrayReverser([1,2,'red','blue']));
